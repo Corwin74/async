8a35ae7e0806b66a1e2f51d3c4b28c63b56accd9
Samokhin Alexander <icorwin74@gmail.com>
2022-12-27 18:59:13 +0300

    Add flashing stars

--- tuts.py
+++ tuts.py
@@ -1,44 +1,55 @@
 import time
 import curses
 import asyncio
+import logging
+import random
+
+
+TIC_TIMEOUT = 0.1
+
+
+async def sleep(canvas, tics=1):
+    for _ in range(int(tics)):
+        await asyncio.sleep(0)
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await asyncio.sleep(0)
+        await sleep(canvas, 20)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        await sleep(canvas, 3)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await asyncio.sleep(0)
+        await sleep(canvas, 5)
 
         canvas.addstr(row, column, symbol)
-        await asyncio.sleep(0)
+        await sleep(canvas, 3)
 
 
-def draw(canvas):
+def draw(canvas, stars_qty=300):
+
+    canvas.border()
+    curses.curs_set(False)
+    max_y, max_x = canvas.getmaxyx()
+    coroutines = [blink(canvas, random.randint(1, max_y-2),
+                        random.randint(1, max_x-2), symbol=random.choice(['*', ':', '+', '.'])) for _ in range(stars_qty)]
     while True:
-        canvas.border()
-        curses.curs_set(False)
-        coroutine = blink(canvas, 5, 20)
-        coroutine.send(None)
-        canvas.refresh()
-        time.sleep(2)
-        coroutine.send(None)
-        canvas.refresh()
-        time.sleep(0.3)
-        coroutine.send(None)
-        canvas.refresh()
-        time.sleep(0.5)
-        coroutine.send(None)
-        canvas.refresh()
-        time.sleep(0.3)
-        canvas.refresh()
-        time.sleep(1)
+        try:
+            for coroutine in coroutines:
+                coroutine.send(None)
+            canvas.refresh()
+            time.sleep(TIC_TIMEOUT)
+        except StopIteration:
+            print('Жопа!')
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__file__)
+    logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level=logging.INFO
+    )
     curses.update_lines_cols()
     curses.wrapper(draw)
